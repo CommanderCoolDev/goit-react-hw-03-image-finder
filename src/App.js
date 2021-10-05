9a150e56f6920be947c71ada7de8e0c97e9b73ff
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-10-05 15:03:20 +0300

    added fetch(maybe))

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import fetchImg from 'services/ApiPixabay';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected'
 }
 
-export default App;
+render() {
+  return(
+    <ToastContainer autoClose={5000}/>
+
+
+  )
+}
