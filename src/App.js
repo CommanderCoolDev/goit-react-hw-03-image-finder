63770c14eaf4c17fc6aea2abe89c242ce4913313
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-10-07 15:28:10 +0300

    added status && loader && such a mess

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ApiPixabay from 'services/ApiPixabay';
 import SearchForm from 'Component/SearchForm/SearchForm';
 import './App.css';
 import ImageGallery from 'Component/ImageGallery/ImageGallery';
+import Button from 'Component/Button/Button';
+import Loader from 'react-loader-spinner';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 // import ImageItem from 'Component/ImageItem/ImageItem';
 
 const Status = {
@@ -34,39 +37,69 @@ export default class App extends Component {
     this.setState({ 
       query: newQuery, 
       page: 1, 
-      images: [] 
+      images: [],
+      
     });
   };
 
   renderImg = () => {
     const { query, page } = this.state;
-    
+    this.setState({ 
+     
+      status: Status.PENDING,
+    });
     ApiPixabay
       .fetchImg(query, page)
-      .then(response =>
+      .then(response =>{
+        const responseLength = response.hits.length;
+    
+        if(responseLength === 0){
+          this.setState({
+            error: new Error(`No search results for ${query}`),
+            status: Status.REJECTED,
+          });
+          return;
+        }
+
         this.setState(prevState => ({
           images: [...prevState.images, ...response.hits],
           page: prevState.page + 1,
-        })),
-      )
+          status: Status.RESOLVED,
+          
+        }))
+      } )
+      
+     
       .catch(error => this.setState({ error, status: Status.REJECTED }))
-      .finally(() => this.setState({ status: Status.RESOLVED }));
+      
   };
+ 
+  
 
  
   render() {
     const { error, status } = this.state;
 
     return(
-      <>
-        <SearchForm onSubmit={this.handleFormSubmit}/>
-        <ToastContainer autoClose={5000} />
-        
-        <ImageGallery images={this.state.images}/>
+      <div className='App'>
+       
 
+        <SearchForm onSubmit={this.handleFormSubmit}></SearchForm>
+        <ToastContainer autoClose={4000} />
+        {status === Status.IDLE && <div><img src='https://cdn.pixabay.com/photo/2016/02/19/11/53/pug-1210025_960_720.jpg' alt=''/></div>}
+        {status === Status.REJECTED && <div>{error.message}<img src='https://cdn.pixabay.com/photo/2016/02/19/11/53/pug-1210025_960_720.jpg' alt=''/></div>}
+        {status === Status.PENDING && (
+          <Loader type="Puff" color="#3fb566" height={100} width={200} timeout={3000}/>
+        )}
+        {status === Status.RESOLVED && (
+     <>
+     <ImageGallery images={this.state.images}/>
+     <Button onClick={this.renderImg}/>
+     </>
+        )}
+   
 
-
-      </>
+    </div>
       
   
   
